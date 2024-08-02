f3744b71a9d3bf17523b6c50eb217df474a4ae6a
VladPlenov <kartraid1@gmail.com>
2024-08-03 00:41:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Menu from './components/Menu';
+import Showcase from './components/Showcase';
+import Services from './components/Services';
+import Designers from './components/Designers';
+import Packages from './components/Packages';
+import Tariffs from './components/Tariffs';
+import Contacts from './components/Contacts';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu/>
+      <div className="main">
+        <h1>Interior Design</h1>
+        <Showcase/>
+        <Services/>
+        <Designers/>
+        <Packages/>
+        <Tariffs/>
+        <Contacts/>
+      </div>
+      <Footer/>
+      
+    </>
+
   );
 }
 
-export default App;
+export default App
